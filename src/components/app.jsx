27577e39031bb0c342956120264eadd1af7f8063
elyseko <elyse1213+github@gmail.com>
2017-01-02 16:03:55 -0800

    chore: fix a tags, should be react router Link components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Banner from './banner';
 
 const App = (props) => {
@@ -6,9 +7,9 @@ const App = (props) => {
     <div>
       <div className="ui fixed inverted menu">
         <h1 className="header item">All Things Westies</h1>
-        <a href="/products" className="item">Products</a>
-        <a href="/cart" className="item">Cart</a>
-        <a href="/blog" className="item">Blog</a>
+        <Link to="/products" className="item">Products</Link>
+        <Link to="/cart" className="item">Cart</Link>
+        <Link to="/blog" className="item">Blog</Link>
       </div>
       <Banner show>
         <h3>Check out the semi-annual sale! Up to 75% off select Items</h3>

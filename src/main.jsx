4197361f33f5b5421adda1da60921c46c8b1ddab
elyseko <elyse1213+github@gmail.com>
2017-01-25 09:37:04 -0800

    feat: skeleton for this section

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
-// render app here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { browserHistory, Router } from 'react-router';
+import sharedRoutes from './shared/sharedRoutes';
+
+ReactDOM.render(
+  <Router routes={sharedRoutes} history={browserHistory} />,
+  document.getElementById('react-content')
+);
--- src/shared/sharedRoutes.jsx
+++ src/shared/sharedRoutes.jsx
@@ -1,1 +1,10 @@
-// routes go here
+import React from 'react';
+import { Route } from 'react-router';
+import App from '../components/app';
+
+const routes = (
+  <Route path="/" component={App}>
+  </Route>
+);
+
+export default routes;

58db18c61e8d588615e10b6bbaee51ddec32f4eb
elyseko <elyse1213+github@gmail.com>
2017-01-25 09:54:42 -0800

    fix: convert to prop back to href

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,9 +5,9 @@ const App = () => {
     <div>
       <div className="ui fixed inverted menu">
         <h1 className="header item">All Things Westies</h1>
-        <a to="/products" className="item">Products</a>
-        <a to="/cart" className="item">Cart</a>
-        <a to="/profile" className="item">Profile</a>
+        <a href="/products" className="item">Products</a>
+        <a href="/cart" className="item">Cart</a>
+        <a href="/profile" className="item">Profile</a>
       </div>
       <div className="ui main text container">
         Content Placeholder

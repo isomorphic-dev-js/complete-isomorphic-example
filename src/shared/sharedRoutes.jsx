f79ebae370fded20d4adfab97296cb29cd059a70
elyseko <elyse1213+github@gmail.com>
2017-01-29 15:29:53 -0800

    feat: add onenter/onchange handlers to react router

--- src/shared/sharedRoutes.jsx
+++ src/shared/sharedRoutes.jsx
@@ -5,8 +5,25 @@ import Cart from '../components/cart';
 import Products from '../components/products';
 import Profile from '../components/profile';
 
+const trackPageView = () => {
+  // In real life you would hook this up to your analytics tool of choice
+  console.log('Tracked a pageview');
+};
+
+const onEnter = () => {
+  // In real life you would hook this up to your analytics tool of choice
+  console.log('OnEnter');
+  trackPageView();
+};
+
+const onChange = () => {
+  // In real life you would hook this up to your analytics tool of choice
+  console.log('OnChange');
+  trackPageView();
+};
+
 const routes = (
-  <Route path="/" component={App}>
+  <Route path="/" component={App} onEnter={onEnter} onChange={onChange}>
     <Route path="/cart" component={Cart} />
     <Route path="/products" component={Products} />
     <Route path="/profile" component={Profile} />

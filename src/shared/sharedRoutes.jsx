6edd505e69f36a7e3f255ffe8f6ef6901042e817
elyseko <elyse1213+github@gmail.com>
2017-01-28 17:25:56 -0800

    feat: add child routes

--- src/shared/sharedRoutes.jsx
+++ src/shared/sharedRoutes.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router';
 import App from '../components/app';
+import Cart from '../components/cart';
+import Products from '../components/products';
+import Profile from '../components/profile';
 
 const routes = (
   <Route path="/" component={App}>
+    <Route path="/cart" component={Cart} />
+    <Route path="/products" component={Products} />
+    <Route path="/profile" component={Profile} />
   </Route>
 );
 

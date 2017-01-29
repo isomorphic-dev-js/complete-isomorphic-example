9c3379df4e61085fa1555eb7299f5d96eaa24927
elyseko <elyse1213+github@gmail.com>
2017-01-28 22:08:14 -0800

    feat: convert links to Link component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const App = (props) => {
   return (
     <div>
       <div className="ui fixed inverted menu">
         <h1 className="header item">All Things Westies</h1>
-        <a href="/products" className="item">Products</a>
-        <a href="/cart" className="item">Cart</a>
-        <a href="/profile" className="item">Profile</a>
+        <Link to="/products" className="item">Products</Link>
+        <Link to="/cart" className="item">Cart</Link>
+        <Link to="/profile" className="item">Profile</Link>
       </div>
       <div className="ui main text container">
         {props.children}

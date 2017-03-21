264affafcce4798cf544113aafa0f55553b6bc16
elyseko <elyse1213+github@gmail.com>
2017-03-21 14:04:03 -0700

    chore: update index route

--- src/shared/sharedRoutes.jsx
+++ src/shared/sharedRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import App from '../components/app';
 import Cart from '../components/cart';
 import Products from '../components/products';
@@ -33,10 +33,11 @@ let beforeRouteRender = (dispatch, prevState, nextState) => {
 export const routes = (onChange = () => {}) => {
   return (
     <Route path="/" component={App} onChange={onChange}>
-      <Route path="/cart" component={Cart} />
-      <Route path="/products" component={Products} />
-      <Route path="/product/detail/:id" component={Detail} />
-      <Route path="/blog" component={Blog} />
+      <IndexRoute component={Products} />
+      <Route path="cart" component={Cart} />
+      <Route path="products" component={Products} />
+      <Route path="product/detail/:id" component={Detail} />
+      <Route path="blog" component={Blog} />
     </Route>
   );
 };

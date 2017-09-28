22711f573eaf70bd45bbcc207e6757878b8bf0b2
elyseko <elyse1213+github@gmail.com>
2017-09-27 22:24:05 -1000

    chore: remove code that is added by reader

--- src/shared/sharedRoutes.jsx
+++ src/shared/sharedRoutes.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from '../components/app';
-import Cart from '../components/cart';
-import Products from '../components/products';
-import Profile from '../components/profile';
 
 const routes = (
   <Route path="/" component={App}>
-    <IndexRoute component={Products} />
-    <Route path="cart" component={Cart} />
-    <Route path="products" component={Products} />
-    <Route path="profile" component={Profile} />
   </Route>
 );
 

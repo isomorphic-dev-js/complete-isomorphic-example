0c32f64aee5a25d2873acf846bf63a831289753c
elyseko <elyse1213+github@gmail.com>
2017-09-28 22:21:06 -1000

    chore: fix prop types issues

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Cart extends Component {
+
   render() {
     return (
       <div className="cart main ui segment">
-        <div className="ui segment divided items">
-          Items will go here.
-        </div>
-        <div className="ui right rail">
-          <div className="ui segment">
-            <span>Total: </span><span>$10</span>
-            <div>Placeholder</div>
-            <button className="ui positive basic button">
-              Checkout
-            </button>
-          </div>
-        </div>
+        Cart!
       </div>
     );
   }
 }
+
+Cart.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      thumbnail: PropTypes.string.isRequired
+    })
+  )
+};
 
 export default Cart;

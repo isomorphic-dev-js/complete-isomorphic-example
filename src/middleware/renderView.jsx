02f925cd1128d9db08687d12bc3fe967ec89d626
elyseko <elyse1213+github@gmail.com>
2017-09-28 22:21:06 -1000

    fix: prop types, out of date code!!!, replace Blog with Profile

--- src/middleware/renderView.jsx
+++ src/middleware/renderView.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { Provider } from 'react-redux';
 import { match, RouterContext } from 'react-router';
+import { Provider } from 'react-redux';
 import routes from '../shared/sharedRoutes';
-import initRedux from '../shared/init-redux.es6';
 import HTML from '../components/html';
+import initRedux from '../shared/init-redux.es6';
 
 export default function renderView(req, res, next) {
   const matchOpts = {
@@ -17,18 +17,17 @@ export default function renderView(req, res, next) {
       let actions = renderProps.components.map((component) => {
         if (component) {
           if (component.displayName &&
-            component.displayName.toLowerCase().indexOf('connect') > -1
+              component.displayName.toLowerCase().indexOf('connect') > -1
           ) {
-            if (component.WrappedComponent.loadData) {
-              return component.WrappedComponent.loadData();
+            if (component.WrappedComponent.prefetchActions) {
+              return component.WrappedComponent.prefetchActions();
             }
-          } else if (component.loadData) {
-            return component.loadData();
+          } else if (component.prefetchActions) {
+            return component.prefetchActions();
           }
         }
         return [];
       });
-
       actions = actions.reduce((flat, toFlatten) => {
         return flat.concat(toFlatten);
       }, []);
@@ -42,14 +41,13 @@ export default function renderView(req, res, next) {
             <RouterContext routes={routes} {...renderProps} />
           </Provider>
         );
-        const html = renderToString(<HTML html={app} />);
-        return res.send(`<!DOCTYPE html>${html}`);
-      }).catch(() => {
-        return next();
+        const html = renderToString(<HTML renderedToStringComponents={app} />);
+        res.send(`<!DOCTYPE html>${html}`);
       });
     } else {
       next();
     }
   };
+
   match(matchOpts, handleMatchResult);
 }

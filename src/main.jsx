28dac4697178e7455c22bb4b79a59fb004d6b99c
elyseko <elyse1213+github@gmail.com>
2017-09-30 10:02:49 -1000

    chore: add missing code from book

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const initialState = JSON.parse(window.__SERIALIZED_STATE__);
+console.log(initialState);
+
 function init() {
   ReactDOM.render(<div>
     Browser Render

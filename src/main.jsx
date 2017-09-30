5f71a2284aa75f508958a243f6240a2e4adf0c99
elyseko <elyse1213+github@gmail.com>
2017-09-30 10:11:43 -1000

    chore: removing code that is added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const initialState = JSON.parse(window.__SERIALIZED_STATE__);
-console.log(initialState);
-
 function init() {
   ReactDOM.render(<div>
     Browser Render

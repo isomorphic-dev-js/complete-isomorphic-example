4e3fa1344e6d133fcc401c846bb74ad2e9543130
elyseko <elyse1213+github@gmail.com>
2017-10-01 00:00:16 -1000

    chore: refactor loadData to prefetchActions

--- src/middleware/renderView.jsx
+++ src/middleware/renderView.jsx
@@ -19,11 +19,11 @@ export default function renderView(req, res, next) {
           if (component.displayName &&
             component.displayName.toLowerCase().indexOf('connect') > -1
           ) {
-            if (component.WrappedComponent.loadData) {
-              return component.WrappedComponent.loadData();
+            if (component.WrappedComponent.prefetchActions) {
+              return component.WrappedComponent.prefetchActions();
             }
-          } else if (component.loadData) {
-            return component.loadData();
+          } else if (component.prefetchActions) {
+            return component.prefetchActions();
           }
         }
         return [];

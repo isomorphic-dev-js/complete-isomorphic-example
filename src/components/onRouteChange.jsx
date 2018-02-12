15c1ee4bbf7e51722d2caa8269af2ad363617e8c
elyseko <elyse1213+github@gmail.com>
2018-02-11 16:41:49 -0800

    chore: cleanup code

--- src/components/onRouteChange.jsx
+++ src/components/onRouteChange.jsx
@@ -51,11 +51,6 @@ const onRouteChange = (WrappedComponent) => {
       return <WrappedComponent {...this.props} />;
     }
   }
-
-  onRouteChange.propTypes = {
-    location: PropTypes.string.isRequired,
-    Component: PropTypes.element.isRequired
-  };
 }
 
 export default onRouteChange;
